591a4ebc1b09e148dab3550061314f9a85a60695
Akhilesh <akhileshpujar796@gmail.com>
2023-07-05 21:45:35 +0530

    sdg

--- app/api/mongo/route.js
+++ app/api/mongo/route.js
@@ -1,31 +1,31 @@
-import {MongoClient} from "mongodb"
-import { NextResponse } from "next/server";
+// import {MongoClient} from "mongodb"
+// import { NextResponse } from "next/server";
 
-export async function GET(request){
+// export async function GET(request){
    
 
-// Replace the uri string with your connection string.
-const uri = "mongodb+srv://stock:nexk0zbtTfyzdJjd@stockmanage.ghxehdr.mongodb.net/";
+// // Replace the uri string with your connection string.
+// const uri = "mongodb+srv://stock:nexk0zbtTfyzdJjd@stockmanage.ghxehdr.mongodb.net/";
 
-const client = new MongoClient(uri);
-
-
-  try {
-    const database = client.db('stockmanage');
-    const movies = database.collection('inventory');
-
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-
-    console.log(movie);
-    return NextResponse.json({"a":34,movie})
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+// const client = new MongoClient(uri);
 
 
+//   try {
+//     const database = client.db('stockmanage');
+//     const movies = database.collection('inventory');
+
+//     // Query for a movie that has the title 'Back to the Future'
+//     const query = { title: 'Back to the Future' };
+//     const movie = await movies.findOne(query);
+
+//     console.log(movie);
+//     return NextResponse.json({"a":34,movie})
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
 
 
-}
+
+
+// }
