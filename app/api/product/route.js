86873a8bc1ae2f84ce795ee99296a1a18fa4f137
Akhilesh <akhileshpujar796@gmail.com>
2023-07-05 21:40:06 +0530

    changs

--- app/api/product/route.js
+++ app/api/product/route.js
@@ -1,53 +1,47 @@
 import {MongoClient} from "mongodb"
-import { NextResponse } from "next/server";
+import { NextResponse } from "next";
 
-export async function GET(request){
+// export async function GET(request){
    
 
-// Replace the uri string with your connection string.
-const uri = "mongodb+srv://stock:nexk0zbtTfyzdJjd@stockmanage.ghxehdr.mongodb.net/";
+// // Replace the uri string with your connection string.
+// const uri = "mongodb+srv://stock:nexk0zbtTfyzdJjd@stockmanage.ghxehdr.mongodb.net/";
 
-const client = new MongoClient(uri);
-  try {
-    const database = client.db('stockmanage');
-    const inventory = database.collection('inventory');
+// const client = new MongoClient(uri);
+//   try {
+//     const database = client.db('stockmanage');
+//     const inventory = database.collection('inventory');
 
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const allproducts = await inventory.findOne(query);
+//     // Query for a movie that has the title 'Back to the Future'
+//     const query = { title: 'Back to the Future' };
+//     const allproducts = await inventory.findOne(query);
 
-    console.log(movie);
-    return NextResponse.json({allproducts})
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-
-
+//     console.log(movie);
+//     return NextResponse.json({allproducts})
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
 
 
-}
+
+
+// }
 
 
 
 export async function POST(request){
    
 
-    // Replace the uri string with your connection string.
-    let body = await request.json()
-    console.log(body)
-    const uri = "mongodb+srv://stock:nexk0zbtTfyzdJjd@stockmanage.ghxehdr.mongodb.net/";
+  const uri = "mongodb+srv://stock:nexk0zbtTfyzdJjd@stockmanage.ghxehdr.mongodb.net/";
+  const client = new MongoClient(uri);
+    let body = request.body;
     
-    const client = new MongoClient(uri);
       try {
-        const database = client.db('stockmanage');
-        const inventory = database.collection('inventory');
-    
-        // Query for a movie that has the title 'Back to the Future'
-        const query = { title: 'Back to the Future' };
-        const product = await inventory.insertOne(body);
-    
-   
+          const database = client.db('stock');
+          const inventory = database.collection('inventory');
+  
+          const product = await inventory.insertOne(body);
         return NextResponse.json({product, ok:true})
       } finally {
         // Ensures that the client will close when you finish/error
